d6624644c145b94b95c62b9a2fbbc3736a0c1d0c
Parth Patel <parth.pmech@gmail.com>
2023-04-19 17:23:46 -0400

    Update .gitignore to avoid .vscode and build

--- build/CMakeFiles/intermittent_model_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/intermittent_model_autogen"
-  "intermittent_model_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/intermittent_model_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
